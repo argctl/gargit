8df54f781578ddc12fd856789e42cf7bc7b18008
argctl <me@gitarg.com>
2024-04-20 21:38:29 -0400

    examples

--- Food.sol
+++ Food.sol
@@ -1,16 +1,28 @@
 pragma solidity ^0.8.24;
 import "./Ingredient.sol";
 
+// item object could be used to require basic facts like container volume and number of containers
+// could just use storage
+
+contract Bottle {
+  string public label = "Pepsi";
+  Food public food;
+  function _food (address food_) {
+    food = Food(food_);
+  }
+}
+
 contract Food {
-
-  constructor (string memory name, string[] memory ingredients, int[] memory amounts,  uint caloric) {
-
+  string public name;
+   
+  constructor (string memory _name, string[] memory recipe, address memory ingredients, int[] memory amounts,  uint caloric) {
+     
   }
   function prepare (address[] memory materials) public returns (uint) {
      
   }
 
-  function consume
+  function consume (
 
 }
 
