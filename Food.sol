655cb653c4a5cdd38dd9fa2ee2eec61c86fb6eea
argctl <me@gitarg.com>
2024-04-28 09:02:41 -0400

    food consumption

--- Food.sol
+++ Food.sol
@@ -4,22 +4,35 @@ import "./Ingredient.sol";
 // item object could be used to require basic facts like container volume and number of containers
 // could just use storage
 
+// Bottle will have label, the label will verify ingredients in a trust network, preperation in a manufacture network
 contract Bottle {
-  string public label = "Pepsi";
+  //string public label = "Pepsi";
+  string public label = "Redbull";
   Food public food;
   function _food (address food_) {
     food = Food(food_);
   }
 }
 
+// Burnable instead of food or food extends burnable?
 contract Food {
   string public name;
-   
-  constructor (string memory _name, string[] memory recipe, address memory ingredients, int[] memory amounts,  uint caloric) {
-     
+  string[] recipe;
+  mapping(string => int) amounts;
+  uint caloric;
+  // we might provide recipe and amounts as a map, but it makes mass difficult in order/execution environment
+  constructor (string memory _name, string[] memory _recipe, int[] memory _amounts,  uint _caloric) {
+    name = _name;
+    recipe = _recipe;
+    caloric = _caloric;
+    for (uint i = 0; i < recipe.length; i++) {
+      amounts[_recipe[i]] = _amounts[i];
+    }
   }
   function prepare (address[] memory materials) public returns (uint) {
-     
+    for (uint i = 0; i < recipe.length; i++) {
+      recipe[i];    
+    }   
   }
 
   function consume (
